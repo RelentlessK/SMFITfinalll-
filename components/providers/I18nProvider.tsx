06ee73d107a343aa999733eb1482a7017f11ed3e
RelentlessK <relentlessb2b@gmail.com>
2025-09-27 16:11:53 +0300

    Fix I18nProvider to properly provide translation context

--- components/providers/I18nProvider.tsx
+++ components/providers/I18nProvider.tsx
@@ -1,14 +1,19 @@
 'use client';
 
 import { ReactNode } from 'react';
-import '@/lib/i18n';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '@/lib/i18n';
 
 interface I18nProviderProps {
   children: ReactNode;
 }
 
 const I18nProvider = ({ children }: I18nProviderProps) => {
-  return <>{children}</>;
+  return (
+    <I18nextProvider i18n={i18n}>
+      {children}
+    </I18nextProvider>
+  );
 };
 
 export default I18nProvider;
